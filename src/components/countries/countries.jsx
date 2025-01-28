2a2d111742975eb57c1aaf37b7d34744ee7b60e4
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-01-28 23:26:08 +0600

    updated the code

--- src/components/countries/countries.jsx
+++ src/components/countries/countries.jsx
@@ -1,5 +1,15 @@
+import { useEffect } from "react";
+import { useState } from "react";
 
 const countries = () => {
+    const [Countries, setCountires] = useState([]);
+
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+            .then(res => res.json())
+            .then(data => console.log(data));
+    }, [])
+
     return (
         <div>
             <h3>countries</h3>
