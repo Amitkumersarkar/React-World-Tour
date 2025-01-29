e5f85b10bb51795501aaec29963a92601cf21f84
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-01-29 09:16:04 +0600

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import './App.css'
-import countries from './components/countries/countries'
 function App() {
 
   return (
     <>
       <h1>React World Tour</h1>
-      <countries></countries>
 
     </>
   )
