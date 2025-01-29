a1dd08907d0e7aa51c960f6d34e665079699a36d
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-01-30 00:16:42 +0600

    added flags.png

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,13 @@
 // imported country.css file
 import './Country.css';
 const Country = ({ country }) => {
-    // console.log(country);
+    console.log(country);
     // destructure the country objects here
-    const { name } = country;
+    const { name, flags } = country;
     return (
         <div className='country'>
             <h3>Name: {name?.common}</h3>
+            <img src={flags.png} alt="" />
         </div>
     );
 };
