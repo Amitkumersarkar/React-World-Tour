0de1c612881747e931be0a7dac98e5c585c16926
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-01-30 00:11:01 +0600

    added updated code

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,9 +1,11 @@
+// imported country.css file
+import './Country.css';
 const Country = ({ country }) => {
     // console.log(country);
     // destructure the country objects here
     const { name } = country;
     return (
-        <div>
+        <div className='country'>
             <h3>Name: {name?.common}</h3>
         </div>
     );
