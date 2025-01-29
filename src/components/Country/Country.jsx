a817a0109367b7893121ba417340ed946baa14ad
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-01-29 23:59:12 +0600

    updated the code

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -4,7 +4,7 @@ const Country = ({ country }) => {
     const { name } = country;
     return (
         <div>
-            <h3>Name: {name.common}</h3>
+            <h3>Name: {name?.common}</h3>
         </div>
     );
 };
